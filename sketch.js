a39f1e7616d4558e28a56481b126c33c95f7a9f9
VallariChavan <vallaric006@gmail.com>
2024-12-10 16:19:56 +0000

    added a yellow circle

--- sketch.js
+++ sketch.js
@@ -3,7 +3,13 @@ function setup() {
 }
 
 function draw() {
+  //Change the background color to whatever you want
   background(220);
 
   // Make a yellow circle
+  noStroke();
+  fill('yellow');
+  ellipse(width/2, height/2, 50);
+
+  
 }
